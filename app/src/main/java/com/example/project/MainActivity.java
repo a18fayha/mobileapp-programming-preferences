1f59ff07c37c7f9e728e365fc5f6a32af2cea76b
a18fayha <a18fayha@student.his.se>
2022-05-05 10:02:07 +0200

    - Added a Button to main activity to start second activity - Implemented Intent to start second activity when button is clicked

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor sharedPrefEdit;
+
+
 
 
     @Override
@@ -14,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        sharedPrefEdit = sharedPreferences.edit();
+
+        btn = findViewById(R.id.secondActivity_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+            }
+        });
     }
 
 }
