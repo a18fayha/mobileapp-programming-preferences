eb43e78c14a67a3f03710f54b3c507fd457ae1dd
a18fayha <62876354+a18fayha@users.noreply.github.com>
2022-05-06 13:52:36 +0200

    In the second activity class i read the data from the shared prefereces created in main activity.

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,39 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Display;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    private TextView firstname, lastname;
+    private String firstNameStr, lastNameStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
+
+
+
+
     }
+    public void onResume(){
+        super.onResume();
+        firstname = findViewById(R.id.first_name);
+        lastname = findViewById(R.id.last_name);
+        SharedPreferences mySharedPref = getSharedPreferences("myPref", MODE_PRIVATE);
+
+        firstNameStr = mySharedPref.getString("förnamn", "");
+        lastNameStr = mySharedPref.getString("efternamn", "");
+
+        firstname.setText(firstNameStr);
+        lastname.setText(lastNameStr);
+
+
+    }
+
+
+
 }
