bb4289f302ff18a1c98e47f8d6096ebade1611ab
a18fayha <62876354+a18fayha@users.noreply.github.com>
2022-05-06 13:51:06 +0200

    - Created my EditTexts and buttons in main activity - Implemented OnlickListeners to the buttons - Created Shared Preference and passed it the input data from the EditText.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.project;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     private Button btn1, btn2;
     private SharedPreferences sharedPreferences;
     private SharedPreferences.Editor sharedPrefEdit;
+    private EditText förnamn, efternamn;
+    private String förnamnStr, efternamnStr;
+
 
 
 
@@ -23,18 +28,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        sharedPreferences = getPreferences(MODE_PRIVATE);
+        förnamn = findViewById(R.id.edit_förnamn);
+        efternamn = findViewById(R.id.edit_efternamn);
+        btn1 = findViewById(R.id.secondActivity_btn);
+        btn2 = findViewById(R.id.save_button);
+        sharedPreferences = getSharedPreferences("myPref",MODE_PRIVATE);
         sharedPrefEdit = sharedPreferences.edit();
 
-        btn1 = findViewById(R.id.secondActivity_btn);
+
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+
+
 
 
             }
         });
+
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                förnamnStr = förnamn.getText().toString();
+                efternamnStr = efternamn.getText().toString();
+                sharedPrefEdit.putString("förnamn", förnamnStr);
+                sharedPrefEdit.putString("efternamn", efternamnStr);
+
+                sharedPrefEdit.apply();
+
+
+
+            }
+        });
+
+
     }
 
 }
